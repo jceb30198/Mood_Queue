465fcfed6382e11141f2d5283b8fc0c38c381336
Jack Ceballos <jackrceballos@gmail.com>
2020-11-10 21:00:13 -0500

    Adding a few notes to the HTML along with playlist.html

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function displayPreview() {
+
+  var queryURL = "http://api.deezer.com/radio";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+
+    var playlist = $("<div class='preview'>");
+
+
+    $('#forecast').empty();
+
+    var results = response.Preview;
+
+    for (var i = 0; i < results.length; i++) {
+
+      var day = Number(results[i].dt_txt.split('-')[2].split(' ')[0]);
+      var hour = results[i].dt_txt.split('-')[2].split(' ')[1];
+    }
+  })
+}
