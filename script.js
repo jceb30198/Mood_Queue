f73785b12613803748a46981f18e2e9d1530dc0c
Tomas Utreras <tutreras100@gmail.com>
2020-11-12 19:52:29 -0500

    new changes

--- script.js
+++ script.js
@@ -29,39 +29,10 @@
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-        console.log(response);
-        music = response.data;
+        console.log(response);       
+        music = response.data[0].preview;
+        console.log(music)
 
-        
-        music.forEach(element => {
-            $.ajax({
-                url: element.tracklist,
-                method: "GET"
-              }).then(function(response2) {
-                console.log(response2)
-                preview = response2.data;
-                var contador = 1;
-                preview.forEach(element2 => {
-                    if(contador <= 5) {
-                        arrayPreview.push({preview: element2.preview});
-                        return;
-                    } 
-                    contador++;
-                });
-              });   
-        });
       });
 
     };
-    $('#happybtn').on('click', function(){
-      alert("test");
-    });
-
-
-    $('#angrybtn').on('click', function(){
-      alert("test");
-    });
-
-    $('#sadbtn').on('click', function(){
-      alert("test");
-    });
